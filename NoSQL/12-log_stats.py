bec6d414cb3c7d0330e6336d01bf499e0334feb7
ItsAlexFS <alexfitzsolis123@gmail.com>
2026-04-17 20:19:18 -0600

    fixed ples

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python3
 """
-Provides some stats about Nginx logs stored in MongoDB
+a Python script that provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
-from typing import List, Dict, Optional
 
 
-def count(collection, options: Dict[str, str] = {}) -> int:
-    """
-    Take a mongoDB collection, filter data and count it
-
-    Args:
-        collection: MongoDB collection
-        options (Dict[str, str]): options to filter
-    """
-    return collection.count_documents(options)
-
-
-if __name__ == '__main__':
-    client = MongoClient(host="localhost", port=27017)
-    collection = client.logs.nginx
-    methods: List[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    print("{:d} logs".format(count(collection)))
-    print("Methods:")
-
-    for method in methods:
-        print("\tmethod {:s}: {:d}".format(
-            method,
-            count(collection, {"method": method})
-        ))
-
-    print("{:d} status check".format(
-        count(collection, {"method": "GET", "path": "/status"})
-    ))
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    x = len(list(nginx_collection.find()))
+    print(x, "logs\nMethods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        print(
+            "\tmethod {}: {}".format(
+                m, len(list(nginx_collection.find({"method": m})))
+                )
+            )
+    print(
+        "{} status check".format(
+            len(list(
+                nginx_collection.find({"method": "GET", "path": "/status"})
+                ))
+            )
+        )
